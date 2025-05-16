5ab997124f922afaf2b1f4c40599c24a1265810b
nazar <nazar10fantom@gmail.com>
2025-05-16 11:36:40 +0200

    update middlware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,25 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
-    const isProductRoute = path.startsWith('/products');
     const authToken = request.cookies.get('authToken')?.value;
 
-    if (isProductRoute && authToken == null) {
-        const loginUrl = new URL('/login', request.url);
-        return NextResponse.redirect(loginUrl);
+    const isProtectedRoute = path.startsWith('/products');
+
+    const isAuthRoute = ['/login'].includes(path);
+
+    if (isProtectedRoute && authToken == null) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (isAuthRoute && authToken != null) {
+        return NextResponse.redirect(new URL('/products', request.url));
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/products/:path*']
+    matcher: ['/products/:path*', '/login']
 };
